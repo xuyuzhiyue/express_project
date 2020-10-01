142fe9819633450edcf29b3a73f60947819a6a44
“e <240011530@qq.com>
2020-10-02 02:13:18 +0800

    修改主题名称

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -107,7 +107,7 @@
       <el-card :body-style="{ padding: '0px' }">
         <img :src="item" class="image">
         <div style="padding: 14px;">
-          <span>好吃的汉堡</span>
+          <span>时尚购物</span>
           <div class="bottom clearfix">
             <time class="time">{{ currentDate }}</time>
             <el-button type="text" class="button">操作按钮</el-button>
